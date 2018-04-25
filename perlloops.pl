ac661bd69c013704c11d71eeceee1109c4a6db12
ksrinuvas <ksrinuvas>
2018-04-25 13:22:56 +0530

    create a new file

---
+++ perlloops.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+#st1
+$n1=25;
+if($n1>25)	{
+	print "number is greater then 25\n";
+}##end if
+
+
+#st2
+$n1=25;
+if($n1>25)	{
+	print "number is greater then 25\n";
+}else	{
+	print "number is not greater then 25\n";
+}
+
+
+#st3
+if($n1>25)	{
+	print "number is greater then 25\n";
+}elsif($n1<25) {
+	print "number is lsser then 25\n";
+}
+else	{
+	print "nember is nither greater then not less then 25\n";
+}
+
+	
+

8bb31fac0c74616eb70056e6f7919cec0560a659
ksrinuvas <ksrinuvas>
2018-04-25 12:31:15 +0530

    create new file

---
+++ test.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+@array=(1,2,3,4,5);
+
+#index numbers in array start with zero
+print"second element:$array[1]\n";
+print"threed element:$array[2]\n";
+
+#updated array
+$array[1]=20;
+$array[2]=40;
+print"updated array:@array\n";
+
+
+

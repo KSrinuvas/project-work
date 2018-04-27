89d8ef5929134c8ff71c0faf8842f47410b9a82a
ksrinuvas <ksrinuvas935>
2018-04-27 09:53:09 +0530

    updated referance

--- ref.pl
+++ ref.pl
@@ -1,9 +1,9 @@
 #!/user/bin/perl
-%hash=('a'=>1,'b'=>2,'c'=>3);
+%hash=('a'=>1,'b'=>2,'c'=>4);
 $hash_ref=\%hash; ##referance
-##$hash_ref={'a'=>1;'b'=>3;'c'=>3};
-##$arr_ref=[1,2,3];
-$arr_ref=[1,2,3];
+##$hash_ref={'a'=>1;'b'=>3;'c'=>4};
+##$arr_ref=[1,2,4];
+$arr_ref=[1,2,4];
 print"$arr_ref\n";
 print"$hash_ref\n";
 %new_hash=%{$hash_ref};##dereferance

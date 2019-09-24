06d398adde4038462a62893bdd1345c0a3d05af1
SRMHP2 <srinuvas935@gmail.com>
2019-09-24 18:07:13 +0530

    this is perl file

---
+++ aa.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+use strict;
+
+
+my $o =`spd-say welcome_to_our_program`;
+
+#my $pp = `spd-say it's_nice_to_meet_you'`;
+
+
+print"it's nice to meet you\n";
+
+
+
+my @arr = (1,2,3,4,5);
+
+my $sum = 0;
+foreach (@arr)	{
+			$sum +=$_;
+}
+
+print"$sum\n";
+
+
